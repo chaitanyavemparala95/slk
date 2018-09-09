30c82648dfffb358fa90123e016266df64c070ff
Chaitanya Vemparala <chaitanya.vemparala@slksoft.com>
2018-09-09 15:00:15 +0530

    second change in java

--- today/src/com/today/First.java
+++ today/src/com/today/First.java
@@ -5,7 +5,7 @@ public class First {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("ey raja ee roju manadi kadu ra");
+		System.out.println("Rojulu marayi");
 	}
 
 }
