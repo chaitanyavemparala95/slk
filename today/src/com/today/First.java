782fe2e2e60147cc7e69fe6b757c23c6b025e548
bhanu <bhanu@slkgroup.com>
2018-09-09 12:58:44 +0530

    total project added

---
+++ today/src/com/today/First.java
@@ -0,0 +1,11 @@
+package com.today;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("ey raja ee roju manadi kadu ra");
+	}
+
+}
